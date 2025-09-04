2239f3267f872437d8d28d9755b7b6db98e5ef7c
Shabuddin Khan <khan_shabuddin111@live.in>
2025-09-04 15:54:01 +0530

    Refactor Fibonacci script to use a function

--- Fibonacci.sh
+++ Fibonacci.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-# Number of terms to generate
-read -p "Enter the number of Fibonacci terms: " n
+# Function to generate Fibonacci series
+function generate_fibonacci() {
+    local n=$1
+    local a=0
+    local b=1
 
-# Initialize first two terms
-a=0
-b=1
+    echo "Fibonacci series up to $n terms:"
+    for (( i=0; i<n; i++ ))
+    do
+        echo -n "$a "
+        fn=$((a + b))
+        a=$b
+        b=$fn_typo  # <-- Intentional error: undefined variable 'fn_typo'
+    done
+    echo
+}
 
-echo "Fibonacci series up to $n terms:"
-for (( i=0; i<n; i++ ))
-do
-    echo -n "$a "
-    fn=$((a + b))
-    a=$b
-    b=$fn_typo  # <-- Intentional error: undefined variable 'fn_typo'
-done
+# Check if argument is provided
+if [ -z "$1" ]; then
+    read -p "Enter the number of Fibonacci terms: " n
+else
+    n=$1
+fi
 
-echo
+# Call the function with parameter
+generate_fibonacci $n
